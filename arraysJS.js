4c0917315e63d946982d2a28825323f1d9a8917e
Martinus1 <matko1711@gmail.com>
2022-03-16 17:48:18 -0400

    first commit

---
+++ arraysJS.js
@@ -0,0 +1,16 @@
+var array1 = [1,2,3,4,5,6,7,8,9,10];
+
+
+
+for(var i = array1.length; i > -1; i--){
+
+	var e = array1[1]
+
+	if (e !== array1[i]) {
+		console.log("false")
+		return false
+	}
+
+}
+	console.log("true")
+
